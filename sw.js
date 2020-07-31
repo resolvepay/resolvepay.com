69a69e8293116bc188f4f225838b42994d5284ea
element364 <element364@users.noreply.github.com>
2020-07-31 13:27:04 +0000

    deploy: resolvepay/resolve-web-gatsby@67e082fa8ef8011b46134afd611557ce9bfe00d9

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ed65b47e496db0d8e1dc.js"
+    "url": "webpack-runtime-e171bbf3a1735aa45fd1.js"
   },
   {
     "url": "framework-835563fd49d1163052b9.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "797270bcb717d029bcd2827e8dc79527"
+    "revision": "ed725e0b429f274e0ee3b4d77f873f78"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-4bac062b324dc69c8b88.js"
